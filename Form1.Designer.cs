90e3143ad67abba706ecd817b08ad6134232be6c
Aayush Mishra <78012607+mishra-aayush21@users.noreply.github.com>
2025-03-11 20:06:55 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace HotelManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRoomNumber = new System.Windows.Forms.TextBox();
+            this.btnAddRoom = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGuestName = new System.Windows.Forms.TextBox();
+            this.btnAddGuest = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbRooms = new System.Windows.Forms.ComboBox();
+            this.cmbGuest = new System.Windows.Forms.ComboBox();
+            this.btnBookRoom = new System.Windows.Forms.Button();
+            this.dgvBookings = new System.Windows.Forms.DataGridView();
+            this.colRoomNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colGuestName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookings)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Room Number";
+            // 
+            // txtRoomNumber
+            // 
+            this.txtRoomNumber.Location = new System.Drawing.Point(229, 28);
+            this.txtRoomNumber.Name = "txtRoomNumber";
+            this.txtRoomNumber.Size = new System.Drawing.Size(149, 20);
+            this.txtRoomNumber.TabIndex = 1;
+            // 
+            // btnAddRoom
+            // 
+            this.btnAddRoom.Location = new System.Drawing.Point(267, 71);
+            this.btnAddRoom.Name = "btnAddRoom";
+            this.btnAddRoom.Size = new System.Drawing.Size(75, 23);
+            this.btnAddRoom.TabIndex = 2;
+            this.btnAddRoom.Text = "Add Room";
+            this.btnAddRoom.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Guest Name";
+            // 
+            // txtGuestName
+            // 
+            this.txtGuestName.Location = new System.Drawing.Point(228, 125);
+            this.txtGuestName.Name = "txtGuestName";
+            this.txtGuestName.Size = new System.Drawing.Size(149, 20);
+            this.txtGuestName.TabIndex = 4;
+            // 
+            // btnAddGuest
+            // 
+            this.btnAddGuest.Location = new System.Drawing.Point(267, 169);
+            this.btnAddGuest.Name = "btnAddGuest";
+            this.btnAddGuest.Size = new System.Drawing.Size(75, 23);
+            this.btnAddGuest.TabIndex = 5;
+            this.btnAddGuest.Text = "Add Guest";
+            this.btnAddGuest.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select Room";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Select Guest";
+            // 
+            // cmbRooms
+            // 
+            this.cmbRooms.FormattingEnabled = true;
+            this.cmbRooms.Location = new System.Drawing.Point(228, 213);
+            this.cmbRooms.Name = "cmbRooms";
+            this.cmbRooms.Size = new System.Drawing.Size(149, 21);
+            this.cmbRooms.TabIndex = 8;
+            this.cmbRooms.SelectedIndexChanged += new System.EventHandler(this.cmbRooms_SelectedIndexChanged);
+            // 
+            // cmbGuest
+            // 
+            this.cmbGuest.FormattingEnabled = true;
+            this.cmbGuest.Location = new System.Drawing.Point(228, 284);
+            this.cmbGuest.Name = "cmbGuest";
+            this.cmbGuest.Size = new System.Drawing.Size(149, 21);
+            this.cmbGuest.TabIndex = 9;
+            // 
+            // btnBookRoom
+            // 
+            this.btnBookRoom.Location = new System.Drawing.Point(267, 334);
+            this.btnBookRoom.Name = "btnBookRoom";
+            this.btnBookRoom.Size = new System.Drawing.Size(75, 23);
+            this.btnBookRoom.TabIndex = 10;
+            this.btnBookRoom.Text = "Book Room";
+            this.btnBookRoom.UseVisualStyleBackColor = true;
+            // 
+            // dgvBookings
+            // 
+            this.dgvBookings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookings.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colRoomNumber,
+            this.colGuestName});
+            this.dgvBookings.Location = new System.Drawing.Point(510, 49);
+            this.dgvBookings.Name = "dgvBookings";
+            this.dgvBookings.Size = new System.Drawing.Size(248, 298);
+            this.dgvBookings.TabIndex = 11;
+            // 
+            // colRoomNumber
+            // 
+            this.colRoomNumber.HeaderText = "Room Number";
+            this.colRoomNumber.Name = "colRoomNumber";
+            // 
+            // colGuestName
+            // 
+            this.colGuestName.HeaderText = "Guest Name";
+            this.colGuestName.Name = "colGuestName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvBookings);
+            this.Controls.Add(this.btnBookRoom);
+            this.Controls.Add(this.cmbGuest);
+            this.Controls.Add(this.cmbRooms);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAddGuest);
+            this.Controls.Add(this.txtGuestName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAddRoom);
+            this.Controls.Add(this.txtRoomNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookings)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRoomNumber;
+        private System.Windows.Forms.Button btnAddRoom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGuestName;
+        private System.Windows.Forms.Button btnAddGuest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbRooms;
+        private System.Windows.Forms.ComboBox cmbGuest;
+        private System.Windows.Forms.Button btnBookRoom;
+        private System.Windows.Forms.DataGridView dgvBookings;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRoomNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colGuestName;
+    }
+}
+
